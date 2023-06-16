fcff718015830fd1de3407162f6b285c50db8eed
MigueXYZ <miguelito.gomes.silva@gmail.com>
2023-06-16 10:19:41 +0100

    Initial Commit

---
+++ src/pt/ipleiria/estg/dei/ei/p2/testepratico/modelo/GestorTemperatura.java
@@ -0,0 +1,19 @@
+package pt.ipleiria.estg.dei.ei.p2.testepratico.modelo;
+
+import java.util.LinkedList;
+
+public class GestorTemperatura {
+    private float temperatura;
+
+    public GestorTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+}
